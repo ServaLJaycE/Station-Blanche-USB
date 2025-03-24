65e5f15ca7efd4ab3f917c471bfd5c100cfc8f75
Anya Lallart <anya.lallart@student.junia.com>
2025-03-24 16:30:50 +0100

    ajout photo

--- Frontend/format_interface.py
+++ Frontend/format_interface.py
@@ -4,6 +4,7 @@ from tkinter import ttk
 from tkinter import * 
 from tkinter.ttk import *
 
+
 # Créer une nouvelle fenêtre pour le formatage
 format_interface = Tk()
 format_interface.title("Choisir le format de la clé USB")
@@ -15,31 +16,36 @@ def fat32():
     # ajouter script pour formater en fat32
     pass
 
-def windows():
+def ntfs():
     # ajouter script pour formater en ntfs
     pass
 
-def linux():
+def ext4():
     # ajouter script pour formater en ext4
     pass
 
 def quitter() :
     format_interface.destroy()
     subprocess.run(["python", "main_interface.py"])
+
+# image
+image_windows = PhotoImage(file="windows.png").subsample(12, 12)   # Image pour NTFS (Windows)
+image_linux = PhotoImage(file="linux.png").subsample(10, 10)   # Image pour EXT4 (Linux)
+image_winlin = PhotoImage(file="winlin.png").subsample(4, 4)   # Image pour FAT32 (Windows et Linux)
     
 # style
 style = ttk.Style() 
 style.configure('TButton', font = ('calibri', 30, 'bold'), borderwidth = '4', relief = 'raised', width = 20, height = 20, padding=(30, 20))
 
 style.configure('Fat32.TButton', foreground='black', background='#AED6F1')   # Bleu
-style.configure('Windows.TButton', foreground='black', background='#ABEBC6')   # Vert
-style.configure('Linux.TButton', foreground='black', background='#F5B7B1')    # Rouge
+style.configure('Ntfs.TButton', foreground='black', background='#ABEBC6')   # Vert
+style.configure('Ext4.TButton', foreground='black', background='#F5B7B1')    # Rouge
 style.configure('Quitter.TButton', foreground='black', background='lightgrey')    # Gris
 
 # Appliquer la couleur au survol avec map()
 style.map('Fat32.TButton', background=[('active', '#5DADE2')]) 
-style.map('Windows.TButton', background=[('active', '#58D68D')])  
-style.map('Linux.TButton', background=[('active', '#EC7063')])  
+style.map('Ntfs.TButton', background=[('active', '#58D68D')])  
+style.map('Ext4.TButton', background=[('active', '#EC7063')])  
 style.map('Quitter.TButton', background=[('active', 'grey')])  
 
 # Ajouter un titre à la nouvelle fenêtre
@@ -54,14 +60,14 @@ button_frame.place(relx=0.5, rely=0.5, anchor="center")
 
 
 # Ajouter les boutons pour choisir le format
-button_fat32 = ttk.Button(button_frame, text="FAT32", command= fat32, style="Fat32.TButton") #  choisir_format("FAT32")
-button_fat32.pack(side=tk.LEFT, padx=15)
+button_fat32 = ttk.Button(button_frame, text="FAT32", image=image_winlin, compound="right", command=fat32, style="Fat32.TButton") # choisir_format("Windows")
+button_fat32.pack(side=tk.LEFT, padx=15, pady=10)
 
-button_windows = ttk.Button(button_frame, text="Windows", command= windows, style="Windows.TButton") # choisir_format("Windows")
-button_windows.pack(side=tk.LEFT, padx=15)
+button_ntfs = ttk.Button(button_frame, text="NTFS", image=image_windows, compound="right", command= ntfs, style="Ntfs.TButton") # choisir_format("Windows")
+button_ntfs.pack(side=tk.LEFT, padx=15)
 
-button_linux = ttk.Button(button_frame, text="Linux", command= linux, style="Linux.TButton") # choisir_format("Linux")
-button_linux.pack(side=tk.LEFT, padx=15)
+button_ext4 = ttk.Button(button_frame, text="EXT4", image=image_linux, compound="right", command= ext4, style="Ext4.TButton") # choisir_format("Linux")
+button_ext4.pack(side=tk.LEFT, padx=15)
 
 button_quitter = ttk.Button(format_interface, text="Quitter", command=quitter, style="Quitter.TButton")
 button_quitter.pack(pady=20)
